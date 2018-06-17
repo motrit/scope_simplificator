872841fe99583047fc1ad692dbbc695830e7a381
Pontifik <motroniii@gmail.com>
2018-06-17 12:09:49 +0300

    Verbose print + minor fixes

--- simplyficator.py
+++ simplyficator.py
@@ -2,7 +2,9 @@
 #-*- coding: utf-8 -*-
 
 import dns.resolver
+import time
 
+verbose = True
 connections = dict()
 
 class Record(object):
@@ -15,9 +17,16 @@ def get_cname_chain(source_domain):
     domain = source_domain
     while True:
         try :
+            time.sleep(1)
             answer = dns.resolver.query(domain, "CNAME")
         except dns.resolver.NoAnswer :
             answer = []
+        except dns.resolver.NXDOMAIN :
+            print ("Domain doesn't exist (NXDOMAIN):", domain)
+            return
+        except dns.exception.Timeout :
+            print ("We got timed out on DNS... Skipping CNAME chain for", source_domain)
+            return
         if len(answer) < 1 :
             return
         elif len(answer) > 1 :
@@ -30,6 +39,8 @@ def get_cname_chain(source_domain):
             elif connections[domain] != str(answer[0]):
                 print ("#INFO# Multiple CNAME records found from another source", domain)
                 return 
+            else :
+                return
 
 def read_hosts(filename):
     hosts = []
@@ -39,7 +50,7 @@ def read_hosts(filename):
 
 def print_record_chain(record, offset = 0):
     print ("|   "*(offset-1) + "|    " * (1 if offset else 0))
-    print ("|   "*(offset-1) + "|--->" * (1 if offset else 0), record.domain)
+    print ("|   "*(offset-1) + "L--->" * (1 if offset else 0), record.domain)
     if record.cname_for :
         for child_index in range(len(record.cname_for)):
             print_record_chain(record.cname_for[child_index], offset + 1)
@@ -48,12 +59,20 @@ def print_record_chain(record, offset = 0):
 
 
 def main():
-    filename = "scope1.txt"
+    filename = "scope.txt"
+    if verbose:
+        print ("Reading hosts from file", filename,'...')
     hosts = read_hosts(filename)
-    root = Record()
+    if verbose:
+        print ("Finished reading. Found", len(hosts), "domains.")
     existing_domains = dict()
-    for item in hosts :
-        get_cname_chain(item)
+    if verbose:
+        print ("Producing connections list...")
+    for index in range(len(hosts)):
+        print("Getting CNAMEs... " + str(index) + "/" + str(len(hosts)) + " done.", end='\r')
+        get_cname_chain(hosts[index])
+    if verbose:
+        print ("\nFinished. Found", len(connections), "connections.")
 
     for host, cname in connections.items() :
         
